f56825c04e43f91c44ada61a68d2310a300b3deb
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-10-29 13:32:29 +0800

    init

---
+++ src/pages/detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <common-header :tittle="tittle" :showmore="true"></common-header>
+    <div class="page-content">
+      <h1 @click="tohome">详情页面</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import commonHeader from 'common/common-header'
+export default {
+  data () {
+    return {
+      tittle: '详情'
+    }
+  },
+  components: {
+    commonHeader
+  },
+  methods: {
+    tohome () {
+      this.$router.goBack()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import "~styles/index.less";
+@import "~styles/variable.less";
+
+</style>
